1ce8b1ed3c23fbc540d101fa64063bfbc8b14cfe
Mustapha <medmus.benlahrech@gmail.com>
2023-11-23 00:48:52 +0100

    add clear

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+name = os.environ.get("USER_NAME", "world")
+
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Dockerized FastAPI!"}
+    return {"message": f"Hello, {name} FastAPI!"}
 
 
 @app.get("/up")
